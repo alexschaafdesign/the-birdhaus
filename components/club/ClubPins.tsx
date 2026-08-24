3868d5dc861e6f9b3813e68b6045b46ebb7f4542
Alex Schaaf <alexschaafdesign@gmail.com>
2026-08-23 21:10:05 -0500

    Club portal: featured Samply embed to 70% height (105vh)

--- components/club/ClubPins.tsx
+++ components/club/ClubPins.tsx
@@ -343,7 +343,7 @@ function AddPinForm({
 // short because extra height just shows blank space.
 function embedHeight(src: string, large: boolean): string {
   if (isVideoEmbed(src)) return 'aspect-video';
-  if (isSamplyEmbed(src)) return large ? 'h-[150vh] min-h-[56rem]' : 'h-96';
+  if (isSamplyEmbed(src)) return large ? 'h-[105vh] min-h-[40rem]' : 'h-96';
   return large ? 'h-64 sm:h-80' : 'h-40';
 }
 
